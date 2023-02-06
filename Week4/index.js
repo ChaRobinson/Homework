380053d51a8e4df51aae169ab1a173b3b733c139
lilbigangel86 <lilbigangel86@yahoo.com>
2023-02-06 16:43:28 -0500

    Section 04 website

---
+++ Week4/index.js
@@ -0,0 +1,7 @@
+console.log("These are all of my P elements", document.getElementsByTagName("p"));
+console.log("This is my FIRST paragraph element", document.querySelector("p"));
+console.log("This is my inner HTML for div"), document.querySelector("div").innerHTML;
+console.log("This is my outer HTML for div"), document.querySelector("div").outerHTML;
+console.log("This is my document's class list for div"), document.querySelector("div").classList;
+console.log("These are ALL of my class lists for div"), document.getElementsByTagName("div").classList;
+
